9c2de97041e6d0a7c1eea0030bb9c9d05bb2ddf6
horlakz <officialhorlakz@gmail.com>
2023-05-24 16:25:48 +0100

    feat(handler/app/): creates handler file for api crud functions

---
+++ handler/app/categoryHandler.go
@@ -0,0 +1,201 @@
+package app
+
+import (
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+
+	dto "github.com/horlakz/energaan-api/database/dto/app"
+	services "github.com/horlakz/energaan-api/database/services/app"
+	"github.com/horlakz/energaan-api/handler"
+	"github.com/horlakz/energaan-api/helper"
+	categoryRequest "github.com/horlakz/energaan-api/payload/request/app/category"
+	"github.com/horlakz/energaan-api/payload/response"
+	validators "github.com/horlakz/energaan-api/validator/app"
+)
+
+type CategoryHandlerInterface interface {
+	IndexHandle(c *fiber.Ctx) error
+	CreateHandle(c *fiber.Ctx) error
+	UpdateHandle(c *fiber.Ctx) error
+	DeleteHandle(c *fiber.Ctx) error
+}
+
+type categoryHandler struct {
+	handler.BaseHandler
+	categoryService   services.CategoryServiceInterface
+	categoryValidator validators.CategoryValidator
+}
+
+func NewCategoryHandler(categoryService services.CategoryServiceInterface) CategoryHandlerInterface {
+	return &categoryHandler{
+		categoryService: categoryService,
+	}
+}
+
+func (handler *categoryHandler) IndexHandle(c *fiber.Ctx) (err error) {
+	var resp response.Response
+	categories, queryError := handler.categoryService.ReadAll()
+
+	if queryError != nil {
+		resp.Status = http.StatusUnprocessableEntity
+		resp.Message = queryError.Error()
+
+		return c.Status(http.StatusUnprocessableEntity).JSON(resp)
+	}
+
+	resp.Status = http.StatusOK
+	resp.Message = http.StatusText(http.StatusOK)
+
+	if len(categories) < 1 {
+		resp.Data = map[string]interface{}{"result": []string{}}
+		return c.Status(http.StatusOK).JSON(resp)
+	}
+
+	resp.Data = map[string]interface{}{"result": categories}
+	return c.Status(http.StatusOK).JSON(resp)
+
+}
+
+func (handler *categoryHandler) CreateHandle(c *fiber.Ctx) (err error) {
+	var resp response.Response
+	var createRequest categoryRequest.CreateRequest
+
+	userID, _ := handler.GetUserID(c)
+	categoryDTO := new(dto.CategoryDTO)
+
+	// parse request body to dto
+	if err := c.BodyParser(&createRequest); err != nil {
+		resp.Status = http.StatusExpectationFailed
+		resp.Message = err.Error()
+
+		return c.Status(fiber.StatusBadRequest).JSON(resp)
+	}
+
+	categoryDTO.Name = createRequest.Name
+	categoryDTO.Slug = helper.CreateSlug(categoryDTO.Name)
+	categoryDTO.CreatedByID = userID
+
+	_, existErr := handler.categoryService.Read(categoryDTO.Slug)
+
+	if existErr == nil {
+		resp.Status = http.StatusConflict
+		resp.Message = "Category Already Exist"
+
+		return c.Status(http.StatusConflict).JSON(resp)
+	}
+
+	vEs, err := handler.categoryValidator.Validate(*categoryDTO)
+
+	if err != nil {
+		resp.Status = http.StatusUnprocessableEntity
+		resp.Message = "validation Error"
+		resp.Data = vEs
+
+		return c.Status(http.StatusUnprocessableEntity).JSON(resp)
+	}
+
+	if _, err := handler.categoryService.Create(*categoryDTO); err != nil {
+		resp.Status = http.StatusInternalServerError
+		resp.Message = err.Error()
+
+		return c.Status(http.StatusInternalServerError).JSON(resp)
+	}
+
+	resp.Status = http.StatusCreated
+	resp.Message = http.StatusText(http.StatusCreated)
+	resp.Data = map[string]interface{}{"result": categoryDTO}
+
+	return c.Status(http.StatusOK).JSON(resp)
+}
+
+func (handler *categoryHandler) UpdateHandle(c *fiber.Ctx) (err error) {
+	var resp response.Response
+	userID, err := handler.GetUserID(c)
+
+	slug := c.Params("slug")
+
+	if err != nil {
+		resp.Status = http.StatusExpectationFailed
+		resp.Message = "Exception Error: " + err.Error()
+
+		return c.Status(http.StatusExpectationFailed).JSON(resp)
+	}
+
+	categoryDto, err := handler.categoryService.Read(slug)
+	categoryDto.Slug = helper.CreateSlug(categoryDto.Name)
+
+	if err != nil {
+		resp.Status = http.StatusNotFound
+		resp.Message = "Record Not Found"
+
+		return c.Status(http.StatusNotFound).JSON(resp)
+	}
+
+	if err := c.BodyParser(&categoryDto); err != nil {
+		resp.Status = http.StatusExpectationFailed
+		resp.Message = err.Error()
+
+		return c.Status(http.StatusExpectationFailed).JSON(resp)
+	}
+
+	categoryDto.Slug = slug
+	categoryDto.UpdatedByID = userID
+
+	vEs, err := handler.categoryValidator.Validate(categoryDto)
+
+	if err != nil {
+		resp.Status = http.StatusUnprocessableEntity
+		resp.Message = "validation Error"
+		resp.Data = vEs
+
+		return c.Status(http.StatusUnprocessableEntity).JSON(resp)
+	}
+
+	if _, err := handler.categoryService.Update(categoryDto); err != nil {
+		resp.Status = http.StatusInternalServerError
+		resp.Message = err.Error()
+
+		return c.Status(http.StatusInternalServerError).JSON(resp)
+	}
+
+	resp.Status = 200
+	resp.Message = "Update Successful"
+
+	return c.Status(200).JSON(resp)
+}
+
+func (handler *categoryHandler) DeleteHandle(c *fiber.Ctx) (err error) {
+
+	var resp response.Response
+	userID, err := handler.GetUserID(c)
+
+	slug := c.Params("slug")
+
+	if err != nil {
+		resp.Status = http.StatusExpectationFailed
+		resp.Message = "Exception Error: " + err.Error()
+
+		return c.Status(http.StatusExpectationFailed).JSON(resp)
+	}
+
+	_, existsErr := handler.categoryService.Read(slug)
+
+	if existsErr != nil {
+		resp.Status = http.StatusNotFound
+		resp.Message = "Record not found"
+		return c.Status(http.StatusNotFound).JSON(resp)
+	}
+
+	if err := handler.categoryService.Delete(userID, slug); err != nil {
+		resp.Status = http.StatusInternalServerError
+		resp.Message = err.Error()
+
+		return c.Status(http.StatusInternalServerError).JSON(resp)
+	}
+
+	resp.Status = http.StatusOK
+	resp.Message = http.StatusText(http.StatusOK)
+
+	return c.Status(http.StatusOK).JSON(resp)
+}
